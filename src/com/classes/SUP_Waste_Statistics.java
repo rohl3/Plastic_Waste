dae1e037625489105869767af1fc9cee357fa656
Rohit <promilavv@gmail.com>
2022-04-28 19:32:20 +0200

    added an image and graph too

--- src/com/classes/SUP_Waste_Statistics.java
+++ src/com/classes/SUP_Waste_Statistics.java
@@ -2,8 +2,6 @@ package com.classes;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
